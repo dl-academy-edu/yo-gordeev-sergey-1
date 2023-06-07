0b58c7af84f56f7a2f6f8d151f204db659dabff7
Sergey <gordeev_sa95@mail.ru>
2023-06-07 22:15:51 +0400

    Fix js
    На проверку

--- js/support.js
+++ js/support.js
@@ -1,6 +1,6 @@
-let suppotModalCloseBtn = document.querySelector('.support-modal__btn')
+let suppotModalCloseBtn = document.querySelector('.support-modal__btn_js')
 let suppotModalOpenBtn = document.querySelector('.banner__btn-item_js')
-let supportModal = document.querySelector('.support-modal_js')
+let supportModal = document.querySelector('.support_js')
 
 suppotModalOpenBtn.addEventListener('click',()=>{
     supportModal.classList.remove('hiden'); 
